633be94c49e0a3fed9ea12c110adc795683b4804
SebasEsp1818 <sebasesgu@gmail.com>
2025-03-27 14:00:42 -0500

    Ejercicio 2

--- src/SRP/Ejercicio_2_Con.kt
+++ src/SRP/Ejercicio_2_Con.kt
@@ -1,2 +1,24 @@
 package SRP
 
+class cliente (val nombre: String, val edad: Int)
+
+class ClienteRepositorio {
+    fun guardar(cliente: Cliente) {
+        println("Guardando cliente ${cliente.nombre} en la base de datos...")
+    }
+}
+
+class NotificadorCorreo {
+    fun enviarCorreo(cliente: Cliente) {
+        println("Enviando correo a ${cliente.nombre}...")
+    }
+}
+
+fun main() {
+    val cliente = Cliente("Carlos", 28)
+    val repositorio = ClienteRepositorio()
+    val notificador = NotificadorCorreo()
+
+    repositorio.guardar(cliente)
+    notificador.enviarCorreo(cliente)
+}
